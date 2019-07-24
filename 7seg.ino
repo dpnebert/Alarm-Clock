61d381182ea131988eb22853f71d9c3f9e35d6d6
dpnebert <dpnebert@gmail.com>
2019-07-24 15:37:33 -0700

    Add files via upload

--- 7seg.ino
+++ 7seg.ino
@@ -1,89 +1,71 @@
-#define onesPin       18
-#define tensPin       19  
+#define onesPin       19
+#define tensPin       18  
 #define hundredsPin   16  
 #define thousandsPin  17 
+bool hex;
+char characters[16]= {              //0 is light on, 1 is light off
+                        0b00000011, //0
+                        0b10011111, //1
+                        0b00100101, //2
+                        0b00001101, //3
+                        0b10011001, //4
+                        0b01001001, //5
+                        0b01000001, //6
+                        0b00011111, //7
+                        0b00000001, //8
+                        0b00011001, //9
+                        0b00010001, //A
+                        0b11000001, //B
+                        0b01100011, //C
+                        0b10000101, //D
+                        0b01100001, //E
+                        0b01110001  //F
 
-int current;
+                        };
+int ones; //time integer, seconds place
+int tens; //time integer, tens place
 void setup() {
-  current = 0;
+
+  Serial.begin(9600);
+  Serial.print("Hello, "); //for debugging
+  
+  Serial.println("World");
+  
+  hex = true;
+  ones = 0;
+  tens = 0;
   // put your setup code here, to run once:
   pinMode(thousandsPin, OUTPUT);
-  digitalWrite(thousandsPin, HIGH);
+  digitalWrite(thousandsPin, LOW);
   pinMode(hundredsPin, OUTPUT);
-  digitalWrite(hundredsPin, HIGH);
+  digitalWrite(hundredsPin, LOW);
   pinMode(onesPin, OUTPUT);
-  digitalWrite(onesPin, HIGH);
+  digitalWrite(onesPin, LOW);
   pinMode(tensPin, OUTPUT);
-  digitalWrite(tensPin, HIGH);
+  digitalWrite(tensPin, LOW);
   
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-
-  
-  digitalWrite(2, HIGH);
-  digitalWrite(3, HIGH);
-  digitalWrite(4, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, HIGH);
-  digitalWrite(9, HIGH);
+  DDRD = 255; //sets data direction register D to be an output
+  PORTD = 255; //when port D is 0 lights are on. 255 lights off
 }
 void loop() {
   
+  digitalWrite(onesPin, HIGH);
+  digitalWrite(tensPin, HIGH);
   
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
   delay(1000);
-  digitalWrite(2, HIGH);
-  digitalWrite(3, HIGH);
-  digitalWrite(4, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, HIGH);
-  digitalWrite(9, HIGH);
-  allOff();
-  if(current == 0)
-  {
-    digitalWrite(thousandsPin, HIGH);
-    current++;
-  } 
-  else if(current == 1)
-  {
-    digitalWrite(hundredsPin, HIGH);
-    current++;
-  } 
-  else if(current == 2)
-  {
-    digitalWrite(onesPin, HIGH);
-    current++;
-  } 
-  else if(current == 3)
-  {
-    digitalWrite(tensPin, HIGH);
-    current = 0;
-  }
-}
-
-void allOff()
-{
-  digitalWrite(thousandsPin, LOW);
-  digitalWrite(hundredsPin, LOW);
-  digitalWrite(onesPin, LOW);
-  digitalWrite(tensPin, LOW);
   
+  displayCharacter(characters[ones]);
+  
+  if(ones == 9)
+  {
+    ones = 0;
+    tens++;
+  }
+  else
+  {
+    ones++;
+  }  
 }
- 
+void displayCharacter(char c) {
+  PORTD = c;
+}
