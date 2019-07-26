27eb785b0abcde224252edca79d4daa5f689b43a
dpnebert <dpnebert@gmail.com>
2019-07-25 21:31:42 -0700

    Add files via upload

--- interrupt_display.ino
+++ interrupt_display.ino
@@ -5,12 +5,11 @@
  * to do so.
  * 
  * It uses PORTB and PORTC for the display.
+ * Four bits to the lower nibble of PORTB and
+ * four bits to the upper nibble of PORTC
  * Select pins are 2, 3, 4, 5
  * 
  */
-
-
-
 
 
 #define selectA       5
@@ -30,9 +29,8 @@
  *      
  *      MUST MAKE BIT ZERO
  *      TO TURN ON LED.
- * 
- *
  */
+ 
 char characters[16]= { //    GFEDCBA
                           0b11000000, //0
                           0b11111001, //1
@@ -51,31 +49,17 @@ char characters[16]= { //    GFEDCBA
                           0b10000110, //E
                           0b10001110, //F
                        };
-
 char displays[] = { selectA, selectB, selectC, selectD };
-
-
-
-
-
-
+int delay_ms;
 bool flag;
 int count;
-void setup() {
-  Serial.begin(9600);
-  flag = false;
-  count = 0;
 
-  initTimer();
-  initPorts();
-  initSelectPins();
-
-  
-  
-}
 void loop() {
   if(flag)
-  {    
+  { 
+    // toggle pin for scope to see clock
+    //PORTD ^= 0b00001000; 
+    
     flag = false;
     updatePortValues(characters[count]);
     
@@ -92,13 +76,12 @@ void loop() {
 
   // This happens ever loop, and all it does it tell
   // which select pin to turn on and then waits
-  // for the delay (second parameter) length of time
-  // before turn off the select pin.  When adding
-  // more select pins, we just need to track their
-  // values above in the if(flag) block, and add
-  // a method call below to do that position after
-  // the ones  
-  pulseCharacterToSelectLEDs(displays[3], 1);
+  // for a length of time before turn off the select
+  // pin.  When adding more select pins, we just
+  // need to track their values above in the if(flag)
+  // block, and add a method call below to do that
+  // position after the ones  
+  pulseCharacterToSelectLEDs(displays[3]);
 }
 
 
@@ -119,22 +102,34 @@ void loop() {
 // 
 // Just read the comments
 
+// Setup look to call all the initialization methods
+void setup() {
+  Serial.begin(9600);
+  delay_ms = 1;
+  flag = false;
+  count = 0;
+
+  initTimer();
+  initPorts();
+  initSelectPins();  
+}
+
+
+
 // Turn the pin 'select' HIGH for the
 // delay_ms length of time, then 
 // turns 'select' back to LOW
 //
-void pulseCharacterToSelectLEDs(int select, int delay_ms)
+void pulseCharacterToSelectLEDs(int select)
 {
   digitalWrite(select, HIGH);
   delay(delay_ms);
   digitalWrite(select, LOW);
-  
 }
 
 // We give it eight bits, and this
 // method reads the lower half into
 // PORTB, and the upper in to PORTC
-//
 void updatePortValues(char character)
 {  
   PORTB = character & 0b00001111; // lower
@@ -149,15 +144,14 @@ ISR(TIMER1_OVF_vect)        // interrupt service routine that wraps a user defin
 }
 void initTimer()
 {
-  // initialize timer1 
-  noInterrupts();           // disable all interrupts
-  TCCR1A = 0;
-  TCCR1B = 0;
+  cli();                    // disable all interrupts
+  TCCR1A = 0;               // Zero out the timer control
+  TCCR1B = 0;               // registers
 
-  TCNT1 = 3036;            // preload timer 65536-16MHz/256/1Hz
-  TCCR1B |= (1 << CS12);    //  1024prescaler 
+  TCNT1 = 3036;             // preload timer 65536-16MHz/256/1Hz
+  TCCR1B |= (1 << CS12);    // 256 prescaler 
   TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
-  interrupts();             // enable all interrupts
+  sei();                    // enable all interrupts
 
 }
 void initPorts()
